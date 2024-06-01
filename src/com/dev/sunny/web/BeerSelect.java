61fbd71bd4ecd23935281e95aa26ac0c3a843f9e
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-06-01 22:25:44 +0530

    Add modification to servlet class
     - Add code changes to get actual beer suggestions based on color from request.

--- src/com/dev/sunny/web/BeerSelect.java
+++ src/com/dev/sunny/web/BeerSelect.java
@@ -1,15 +1,26 @@
 package com.dev.sunny.web;
 
+import com.dev.sunny.model.BeerExpert;
+
 import java.io.*;
+import java.util.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class BeerSelect extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String c = request.getParameter("color");
+        BeerExpert be = new BeerExpert();
+        List<String> result = be.getBrands(c);
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("Beer Selection Advice<br>");
-        String c = request.getParameter("color");
-        out.println("<br>Got beer color " + c);
+
+        Iterator it = result.iterator();
+        while(it.hasNext()) {
+            out.println("<br>try: " + it.next());
+        }
     }
 }
